ef4d542bbe95721c3b065d951b2fa14392f93410
Hansle Lee <hssarah13@gmail.com>
2021-07-29 17:23:12 +0900

    Initial commit

---
+++ src/main/java/readingcord/readingcordapi/ReadingcordApiApplication.java
@@ -0,0 +1,13 @@
+package readingcord.readingcordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingcordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingcordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/readingcord/readingcordapi/ReadingcordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package readingcord.readingcordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingcordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
